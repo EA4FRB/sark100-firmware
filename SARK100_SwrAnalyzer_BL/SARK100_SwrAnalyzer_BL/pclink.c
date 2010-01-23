680a688ffb07b2220acbcb6b0053af50303ca142
Melchor Varela <37381609+EA4FRB@users.noreply.github.com>
2010-01-23 22:12:19 +0000

    Implements scan and scanr commands

--- SARK100_SwrAnalyzer_BL/SARK100_SwrAnalyzer_BL/pclink.c
+++ SARK100_SwrAnalyzer_BL/SARK100_SwrAnalyzer_BL/pclink.c
@@ -46,6 +46,7 @@
 #include "glb_data.h"
 #include "calibrate_reflectometer.h"
 #include "calcs.h"
+#include "util.h"
 
 //-----------------------------------------------------------------------------
 //  Private data:
@@ -55,24 +56,21 @@ static BYTE const gszCmdOff		[] = "off";
 static BYTE const gszCmdFreq	[] = "freq";
 static BYTE const gszCmdMeasImp	[] = "imp";
 static BYTE const gszCmdMeasRaw	[] = "raw";
+static BYTE const gszCmdScan	[] = "scan";
+static BYTE const gszCmdScanRaw	[] = "scanr";
 
-static BYTE const gszWelcome	[] = "\r\nSARK100 SWR Analyzer V0.2 \r\n";
+static BYTE const gszWelcome	[] = "\r\nSARK100 SWR Analyzer V0.3\r\n";
 static BYTE const gszCmdPrompt	[] = "\r\n>>";
 static BYTE const gszWaitLink	[] = "Waiting Link";
 static BYTE const gszOk			[] = "\r\nOK\r\n";
-static BYTE const gszSwr		[] = "\r\nSWR=";
-static BYTE const gszSwrOvf		[] = ">10";
-static BYTE const gszZ			[] = "\tZ=";
-static BYTE const gszR			[] = "\tR=";
-static BYTE const gszX			[] = "\tX=";
-static BYTE const gszOvf		[] = "Ovf";
-static BYTE const gszVf			[] = "\r\nVf=";
-static BYTE const gszVr			[] = "\tVr=";
-static BYTE const gszVz			[] = "\tVz=";
-static BYTE const gszVa			[] = "\tVa=";
+static BYTE const gszSwrOvf		[] = "9.99";
+static BYTE const gszOvf		[] = "9999";
+static BYTE const gszStart		[] = "\r\nStart\r\n";
+static BYTE const gszEnd		[] = "End\r\n";
 
 static BYTE const gszErrFreqNotSet	[] = "\r\nError: freq not set\r\n";
 static BYTE const gszErrExpectFreq	[] = "\r\nError: expected freq val\r\n";
+static BYTE const gszErrExpectStep	[] = "\r\nError: expected step val\r\n";
 static BYTE const gszErrInvalidFreq	[] = "\r\nError: invalid freq\r\n";
 static BYTE const gszErrCmdNotFound1[] = "\r\nCommand <";
 static BYTE const gszErrCmdNotFound2[] = "> not found\r\n";
@@ -81,6 +79,17 @@ static BYTE const gszErrCmdNotFound2[] = "> not found\r\n";
 //  Prototypes
 //-----------------------------------------------------------------------------
 static BYTE GetBand (DWORD dwFreq);
+static void Cmd_Off (void);
+static void Cmd_On (void);
+static void Cmd_Freq (void);
+static void Cmd_Imp (void);
+static void Cmd_Raw (void);
+static void Cmd_Scan (BYTE bRaw);
+
+//-----------------------------------------------------------------------------
+//  Private data
+//-----------------------------------------------------------------------------
+static DWORD gdwCurrentFreq = -1;
 
 //-----------------------------------------------------------------------------
 //  FUNCTION NAME:	PcLink
@@ -99,9 +108,6 @@ static BYTE GetBand (DWORD dwFreq);
 void PcLink (void)
 {
 	char * strPtr; 						// Parameter pointer
-	DWORD dwCurrentFreq = -1;
-	BYTE szMsg[20];
-	BYTE bBand;
 
 	UART_CmdReset(); 					// Initialize receiver/cmd buffer
 	UART_IntCntl(UART_ENABLE_RX_INT); 	// Enable RX interrupts
@@ -130,114 +136,33 @@ void PcLink (void)
 				LCD_PrString(strPtr);
     			if(!cstrcmp((const char*)gszCmdOn,(char*)strPtr))
 				{
-					if (dwCurrentFreq==-1)
-					{
-						UART_CPutString(gszErrFreqNotSet);
-						break;
-					}
-					DDS_Set(dwCurrentFreq);
-					UART_CPutString(gszOk);
+					Cmd_On();
 				}
     			else if(!cstrcmp((const char*)gszCmdOff,(char*)strPtr))
 				{
-					DDS_Set(0);
-					UART_CPutString(gszOk);
+					Cmd_Off();
+				}
+    			else if(!cstrcmp((const char*)gszCmdScan,(char*)strPtr))
+    			{
+					Cmd_Scan(FALSE);
+				}
+    			else if(!cstrcmp((const char*)gszCmdScanRaw,(char*)strPtr))
+    			{
+					Cmd_Scan(TRUE);
 				}
     			else if(!cstrcmp((const char*)gszCmdFreq,(char*)strPtr))
 				{
-					do
-					{
-						strPtr = UART_szGetParam();
-						if (strPtr==NULL)
-						{
-							UART_CPutString(gszErrExpectFreq);
-							break;
-						}
-						dwCurrentFreq = atol(strPtr);
-						bBand = GetBand(dwCurrentFreq);
-						if (bBand==-1)
-						{
-							UART_CPutString(gszErrInvalidFreq);
-							break;
-						}
-						g_xBridgeCorrect = g_xBandCorrFactor[bBand];
-						Adjust_Dds_Gain(bBand);
-						UART_CPutString(gszOk);
-					} while (FALSE);
+					Cmd_Freq();
 				}
     			else if(!cstrcmp((const char*)gszCmdMeasImp,(char*)strPtr))
 				{
-					Do_Measure();
-					gwSwr = Calculate_Swr(g_xBridgeMeasure.Vf, g_xBridgeMeasure.Vr);
-					gwZ = Calculate_Z(gwSwr, g_xBridgeMeasure.Vz, g_xBridgeMeasure.Va);
-					gwR = Calculate_R(gwZ, gwSwr);
-					gwX = Calculate_X(gwZ, gwR);
-
-					itoa(szMsg, gwSwr, 10);
-					UART_CPutString(gszSwr);
-					if (gwSwr>SWR_MAX)
-					{
-						UART_CPutString(gszSwrOvf);
-					}
-					else
-					{
-						UART_PutString(szMsg);
-					}
-
-					itoa(szMsg, gwZ, 10);
-					UART_CPutString(gszZ);
-					if (gwZ==-1)
-					{
-						UART_CPutString(gszOvf);
-					}
-					else
-					{
-						UART_PutString(szMsg);
-					}
-
-					itoa(szMsg, gwR, 10);
-					UART_CPutString(gszR);
-					if (gwR==-1)
-					{
-						UART_CPutString(gszOvf);
-					}
-					else
-					{
-						UART_PutString(szMsg);
-					}
-
-					itoa(szMsg, gwX, 10);
-					UART_CPutString(gszX);
-					if (gwX==-1)
-					{
-						UART_CPutString(gszOvf);
-					}
-					else
-					{
-						UART_PutString(szMsg);
-					}
 					UART_PutCRLF();
+					Cmd_Imp();
 				}
     			else if(!cstrcmp((const char*)gszCmdMeasRaw,(char*)strPtr))
 				{
-					Do_Measure();
-
-					ltoa(szMsg, g_xBridgeMeasure.Vf/100, 10);
-					UART_CPutString(gszVf);
-					UART_PutString(szMsg);
-
-					ltoa(szMsg, g_xBridgeMeasure.Vr/100, 10);
-					UART_CPutString(gszVr);
-					UART_PutString(szMsg);
-
-					ltoa(szMsg, g_xBridgeMeasure.Vz/100, 10);
-					UART_CPutString(gszVz);
-					UART_PutString(szMsg);
-
-					ltoa(szMsg, g_xBridgeMeasure.Va/100, 10);
-					UART_CPutString(gszVa);
-					UART_PutString(szMsg);
 					UART_PutCRLF();
+					Cmd_Raw();
 				}
 				else
 				{
@@ -283,3 +208,270 @@ static BYTE GetBand (DWORD dwFreq)
 
 	return bBand;
 }
+
+//-----------------------------------------------------------------------------
+//  FUNCTION NAME:	Cmd_On
+//
+//  DESCRIPTION:
+//
+//	Process on (enable DDS) command
+//
+//  ARGUMENTS:
+//     none.
+//
+//  RETURNS:
+//     none.
+//
+//-----------------------------------------------------------------------------
+static void Cmd_On (void)
+{
+	if (gdwCurrentFreq==-1)
+	{
+		UART_CPutString(gszErrFreqNotSet);
+	}
+	else
+	{
+		DDS_Set(gdwCurrentFreq);
+		UART_CPutString(gszOk);
+	}		
+}
+
+//-----------------------------------------------------------------------------
+//  FUNCTION NAME:	Cmd_Off
+//
+//  DESCRIPTION:
+//
+//	Process on command
+//
+//  ARGUMENTS:
+//     none.
+//
+//  RETURNS:
+//     none.
+//
+//-----------------------------------------------------------------------------
+static void Cmd_Off (void)
+{
+	DDS_Set(0);
+	UART_CPutString(gszOk);
+}
+
+//-----------------------------------------------------------------------------
+//  FUNCTION NAME:	Cmd_Freq
+//
+//  DESCRIPTION:
+//
+//	Process freq (set frequency) command
+//
+//  ARGUMENTS:
+//     none.
+//
+//  RETURNS:
+//     none.
+//
+//-----------------------------------------------------------------------------
+static void Cmd_Freq (void)
+{
+	char * strPtr; 						// Parameter pointer
+	BYTE bBand;
+
+	do
+	{
+		strPtr = UART_szGetParam();
+		if (strPtr==NULL)
+		{
+			UART_CPutString(gszErrExpectFreq);
+			break;
+		}
+		gdwCurrentFreq = atol(strPtr);
+		bBand = GetBand(gdwCurrentFreq);
+		if (bBand==-1)
+		{
+			UART_CPutString(gszErrInvalidFreq);
+			break;
+		}
+		g_xBridgeCorrect = g_xBandCorrFactor[bBand];
+		Adjust_Dds_Gain(bBand);
+		UART_CPutString(gszOk);
+	} while (FALSE);
+}
+
+//-----------------------------------------------------------------------------
+//  FUNCTION NAME:	Cmd_Imp
+//
+//  DESCRIPTION:
+//
+//	Process imp (impedance) command
+//
+//  ARGUMENTS:
+//     none.
+//
+//  RETURNS:
+//     none.
+//
+//-----------------------------------------------------------------------------
+static void Cmd_Imp (void)
+{
+	BYTE szMsg[20];
+
+	Do_Measure();
+	gwSwr = Calculate_Swr(g_xBridgeMeasure.Vf, g_xBridgeMeasure.Vr);
+	gwZ = Calculate_Z(g_xBridgeMeasure.Vz, g_xBridgeMeasure.Va);
+	gwR = Calculate_R(gwZ, gwSwr);
+	gwX = Calculate_X(gwZ, gwR);
+
+	if (gwSwr>=SWR_MAX)
+	{
+		gwZ = 9999;
+		gwR = 9999;
+		gwX = 9999;
+	}
+	
+	itoa(szMsg, gwSwr, 10);
+	UART_PutChar( szMsg[0] );
+	UART_PutChar( '.' );
+	UART_PutChar( szMsg[1] );
+	UART_PutChar( szMsg[2] );
+	
+	UART_PutChar(',');
+	itoa(szMsg, gwR, 10);
+	UART_PutString(szMsg);
+
+	UART_PutChar(',');
+	itoa(szMsg, gwX, 10);
+	UART_PutString(szMsg);
+
+	UART_PutChar(',');
+	itoa(szMsg, gwZ, 10);
+	UART_PutString(szMsg);
+	
+	UART_PutCRLF();
+}
+
+//-----------------------------------------------------------------------------
+//  FUNCTION NAME:	Cmd_Raw
+//
+//  DESCRIPTION:
+//
+//	Process raw (raw measurement data) command
+//
+//  ARGUMENTS:
+//     none.
+//
+//  RETURNS:
+//     none.
+//
+//-----------------------------------------------------------------------------
+static void Cmd_Raw (void)
+{
+	BYTE szMsg[20];
+
+	Do_Measure();
+
+	ltoa(szMsg, g_xBridgeMeasure.Vf, 10);
+	UART_PutString(szMsg);
+
+	UART_PutChar(',');
+	ltoa(szMsg, g_xBridgeMeasure.Vr, 10);
+	UART_PutString(szMsg);
+
+	UART_PutChar(',');
+	ltoa(szMsg, g_xBridgeMeasure.Vz, 10);
+	UART_PutString(szMsg);
+
+	UART_PutChar(',');
+	ltoa(szMsg, g_xBridgeMeasure.Va, 10);
+	UART_PutString(szMsg);
+
+	UART_PutCRLF();
+}
+
+//-----------------------------------------------------------------------------
+//  FUNCTION NAME:	Cmd_Scan
+//
+//  DESCRIPTION:
+//
+//	Process scan command
+//
+//  ARGUMENTS:
+//     none.
+//
+//  RETURNS:
+//     none.
+//
+//-----------------------------------------------------------------------------
+static void Cmd_Scan (BYTE bRaw)
+{
+	DWORD dwFreq;
+	DWORD dwEndFreq;
+	DWORD dwLimitFreq;
+	DWORD dwStepFreq;
+	BYTE bBand;
+	BYTE bBandEnd;
+	char * strPtr; 						// Parameter pointer
+
+	do
+	{
+										// Get start frequency
+		strPtr = UART_szGetParam();
+		if (strPtr==NULL)
+		{
+			UART_CPutString(gszErrExpectFreq);
+			break;
+		}
+		dwFreq = atol(strPtr);
+		bBand = GetBand(dwFreq);
+		if (bBand==-1)
+		{
+			UART_CPutString(gszErrInvalidFreq);
+			break;
+		}
+										// Get end frequency
+		strPtr = UART_szGetParam();
+		if (strPtr==NULL)
+		{
+			UART_CPutString(gszErrExpectFreq);
+			break;
+		}
+		dwEndFreq = atol(strPtr);
+		bBandEnd = GetBand(dwEndFreq);
+		if (bBandEnd==-1)
+		{
+			UART_CPutString(gszErrInvalidFreq);
+			break;
+		}
+
+										// Get step value
+		strPtr = UART_szGetParam();
+		if (strPtr==NULL)
+		{
+			UART_CPutString(gszErrExpectStep);
+			break;
+		}
+		dwStepFreq = atoi(strPtr);
+
+		UART_CPutString(gszStart);
+		for (; dwFreq < dwEndFreq; bBand++)
+		{
+			g_xBridgeCorrect = g_xBandCorrFactor[bBand];
+			Adjust_Dds_Gain(bBand);
+			dwLimitFreq = g_xBandLimits[bBand].high * BAND_FREQ_MULT;
+
+			do
+			{
+				DDS_Set(dwFreq);
+				if (bRaw)
+					Cmd_Raw();
+				else
+					Cmd_Imp();
+				dwFreq += dwStepFreq;
+			} while ((dwFreq < dwLimitFreq) && (dwFreq < dwEndFreq));
+		}
+		UART_CPutString(gszEnd);
+		if (gdwCurrentFreq==-1)
+		{
+			DDS_Set(0);
+		}
+	} while (FALSE);
+}
+
